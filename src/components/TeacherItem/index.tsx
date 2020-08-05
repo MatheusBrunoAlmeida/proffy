eb61a9c3a296e87ce2b09c3f4eeb8eedf3d4176a
Matheus Bruno Almeida <manancialsup@hotmail.com>
2020-08-05 01:08:10 -0300

    hello word

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+
+import './styles.css'
+
+function TeacherItem() {
+    return (
+        <article className="teacher-item">
+            <header>
+                <img src="https://avatars3.githubusercontent.com/u/50282082?s=460&u=1e7ecb200ed685af12b01f22df04020170fbc203&v=4" alt="Matheus Bruno Almeida" />
+                <div>
+                    <strong>Matheus Bruno Almeida</strong>
+                    <span>Quimica</span>
+                </div>
+            </header>
+
+            <p>
+                Entusiasta das melhores tecnologias de química avançada.
+                        <br /><br />
+                        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+                    </p>
+
+            <footer>
+                <p>
+                    Preço/hora
+                            <strong>R$ 80,00</strong>
+                </p>
+                <button type="button">
+                    <img src={whatsappIcon} alt="Whatsapp" />
+                            Entrar em contato
+                        </button>
+            </footer>
+        </article>
+    )
+}
+
+export default TeacherItem;
